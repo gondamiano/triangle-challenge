1ab1d9da6c0ff411f27fb12851136dc1c95daf78
gonzalo damiano <gondamiano@gmail.com>
2019-01-14 15:09:34 -0300

    Fix some conditions inside the main function. Delete the function call.

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ import TriangleTypes from './Messages/TriangleTypes';
   if (sideA == undefined || sideB == undefined || sideC == undefined) {
       throw new Error(Errors.nullOrUndefined);
   }
-
   checkInputValues(...arguments);
 
   if(sideA === sideB && sideA === sideC) {
-    console.log(`You form a ${TriangleTypes.equilateral} triangle`);
+    console.log(`You formed a ${TriangleTypes.equilateral} triangle`);
   }
 
   else if(sideA === sideB || sideA === sideC || sideB === sideC) {
-    console.log(`You form a ${TriangleTypes.Isosceles} triangle`);
+    console.log(`You formed a ${TriangleTypes.Isosceles} triangle`);
   }
 
-  else console.log(`You form a ${TriangleTypes.Scalene} triangle`);
+  else console.log(`You formed a ${TriangleTypes.Scalene} triangle`);
 }
 
 function checkInputValues(sideA, sideB, sideC){
@@ -25,7 +24,7 @@ function checkInputValues(sideA, sideB, sideC){
       throw new Error(Errors.parametersLength);
   }
 
-  if(!args.every(item => typeof item === "number")) {
+  if(!args.every(item => typeof item === "number" && Number.isFinite(item))) {
       throw new Error(Errors.argumentsType);
   }
 
@@ -36,7 +35,4 @@ function checkInputValues(sideA, sideB, sideC){
   if(sideA + sideB < sideC || sideA + sideC < sideB || sideB + sideC < sideA) {
     throw new Error(Errors.unabledToFormTriangle);
   }
-
 }
-
-checkTriangleType(25,25,25);
